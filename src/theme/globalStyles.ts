0094bf2f7c582beac3adc82fb80f2c24a8e8bebe
Sebastian2806 <sebastol90@wp.pl>
2021-03-28 21:56:28 +0200

    change import in global style

--- src/theme/globalStyles.ts
+++ src/theme/globalStyles.ts
@@ -1,23 +1,35 @@
-import { createGlobalStyle } from 'styled-components';
+import * as styled from 'styled-components';
 
-export const GlobalStyles = createGlobalStyle`
-    html, body, p, h1, h2, h3, h4, h5, h6, button, input {
-        margin: 0;
-        padding: 0;
-    }
+export const GlobalStyles = styled.createGlobalStyle`
+  html,
+  body,
+  p,
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  button,
+  input {
+    margin: 0;
+    padding: 0;
+  }
 
-    ol, ul {
-        list-style: none;
-    }
+  ol,
+  ul {
+    list-style: none;
+  }
 
-    body {
-        background-color: ${({ theme }) => theme.colors.background};
-        font-family:  ${({ theme }) => theme.fonts.roboto};
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-    }
+  body {
+    background-color: ${({ theme }) => theme.colors.background};
+    font-family: ${({ theme }) => theme.fonts.roboto};
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
 
-    button, input {
-        font-family:  ${({ theme }) => theme.fonts.roboto};
-    }
+  button,
+  input {
+    font-family: ${({ theme }) => theme.fonts.roboto};
+  }
 `;
