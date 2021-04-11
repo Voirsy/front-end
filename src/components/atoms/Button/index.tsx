b95356031b45a3855bcf3b70ccb402e5fdc76e09
zawolanski <sebastian.zawolanski@gmail.com>
2021-04-11 19:21:03 +0200

    feat: create button atom

--- src/components/atoms/Button/index.tsx
+++ src/components/atoms/Button/index.tsx
@@ -1,9 +1,43 @@
+import BeatLoader from 'react-spinners/BeatLoader';
 import { Props } from './types';
 import Styled from './styles';
+import theme from '../../../theme/theme';
 
-const Button = ({ label, isPrimary = false, isDanger = false }: Props) => (
-  <Styled.Button isPrimary={isPrimary} isDanger={isDanger} type="button">
-    {label}
+const Button = ({
+  label,
+  type = 'button',
+  size = 'small',
+  borderRadius = 1,
+  isPrimary = true,
+  isWarning = false,
+  border = true,
+  isPrimaryColor = false,
+  isBold = false,
+  buttonWidth = 0,
+  isLoading = false,
+  onClick = () => {},
+}: Props) => (
+  <Styled.Button
+    type={type}
+    size={size}
+    borderRadius={borderRadius}
+    isPrimary={isPrimary}
+    isWarning={isWarning}
+    border={border}
+    isPrimaryColor={isPrimaryColor}
+    isBold={isBold}
+    buttonWidth={buttonWidth}
+    isLoading={isLoading}
+    onClick={onClick}
+  >
+    {isLoading ? (
+      <BeatLoader
+        color={theme.colors.grayColors.dark}
+        size={size === 'small' || size === 'verysmall' ? 12 : 16}
+      />
+    ) : (
+      label
+    )}
   </Styled.Button>
 );
 
--- src/components/atoms/Button/types.ts
+++ src/components/atoms/Button/types.ts
@@ -1,5 +1,15 @@
-export interface Props {
-  isPrimary?: boolean;
-  isDanger?: boolean;
+export type Props = {
   label: string;
-}
+  borderRadius?: number;
+  type?: 'button' | 'submit' | 'reset';
+  size?: 'verysmall' | 'small' | 'medium' | 'large';
+  isPrimary?: boolean;
+  isWarning?: boolean;
+  border?: boolean;
+  isPrimaryColor?: boolean;
+  isBold?: boolean;
+  buttonWidth?: number;
+  isLoading?: boolean;
+  // eslint-disable-next-line no-unused-vars
+  onClick: (...args: any[]) => any;
+};
