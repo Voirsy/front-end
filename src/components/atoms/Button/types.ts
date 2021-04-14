80ce3c283ddccc0a284c662a2ee920575d18639b
sebastian.zawolanski <sebastian.zawolanski@gmail.com>
2021-04-14 22:07:51 +0200

    feat: add aria-label to Button

--- src/components/atoms/Button/types.ts
+++ src/components/atoms/Button/types.ts
@@ -1,5 +1,6 @@
 export type Props = {
   label: string;
+  ariaLabel?: string;
   borderRadius?: number;
   type?: 'button' | 'submit' | 'reset';
   size?: 'verysmall' | 'small' | 'medium' | 'large';
