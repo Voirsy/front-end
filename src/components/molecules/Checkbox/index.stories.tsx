cb02d9186c3449efe1952a45daf59514ecc6b52d
Sebastian <sebastian.zawolanski@gmail.com>
2021-04-24 18:33:00 +0200

    docs: add documentation to Checkbox storie

--- src/components/molecules/Checkbox/index.stories.tsx
+++ src/components/molecules/Checkbox/index.stories.tsx
@@ -5,6 +5,17 @@ import { CheckboxProps } from './types';
 export default {
   title: 'Components/Checkbox',
   component: Checkbox,
+  argTypes: {
+    name: {
+      description: 'Name of input. Name is also used for `id` and `htmlFor`',
+    },
+    size: {
+      description: "Width and height of CHECKBOX! in rem's ",
+    },
+    children: {
+      description: 'Input label',
+    },
+  },
 } as Meta;
 
 const Template: Story<CheckboxProps> = (args) => <Checkbox {...args} />;
