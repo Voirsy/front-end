1de4a01d2f292ce3ff9070ae0acc8a4ba48e2fdd
Sebastian <sebastian.zawolanski@gmail.com>
2021-04-27 10:14:41 +0200

    docs: add documentation to spinner storie

--- src/components/atoms/Spinner/index.stories.tsx
+++ src/components/atoms/Spinner/index.stories.tsx
@@ -5,6 +5,16 @@ import { SpinnerProps } from './types';
 export default {
   title: 'Components/Spinner',
   component: Spinner,
+  argTypes: {
+    size: {
+      description: 'Size of spinner. Always pass value with unit e.g.: `25px`, `70%` or `4rem`',
+      defaultValue: '3rem',
+    },
+    color: {
+      description: 'Color of spinner',
+      defaultValue: 'primary',
+    },
+  },
 } as Meta;
 
 const Template: Story<SpinnerProps> = (args) => <Spinner {...args} />;
