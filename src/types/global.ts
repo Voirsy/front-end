fd99d55830637fb566891a965f83418c41e1c064
Sebastian <sebastian.zawolanski@gmail.com>
2021-04-27 10:18:54 +0200

    feat: add types to global.ts

--- src/types/global.ts
+++ src/types/global.ts
@@ -1,3 +1,21 @@
 export type Variant = 'default' | 'withDisable' | 'login' | 'placeholder' | 'animate' | 'search';
 export type InputType = 'text' | 'password' | 'email' | 'search' | 'textarea';
 export type ButtonType = 'button' | 'submit' | 'reset';
+export type Color =
+  | 'primary'
+  | 'secondary'
+  | 'warning'
+  | 'social'
+  | 'black'
+  | 'darkgray'
+  | 'lightblue';
+
+export enum Colors {
+  primary = 'purple',
+  secondary = 'grayColors',
+  warning = 'red',
+  social = 'social',
+  black = 'black',
+  darkgray = 'dark',
+  lightblue = 'blueOutline',
+}
