3e1183e173b8cbc73e987be317c3659b06b4f9aa
Sebastian <sebastian.zawolanski@gmail.com>
2021-05-02 17:44:26 +0200

    feat: apply custom components to Header template

--- src/components/molecules/ProfileMenu/index.tsx
+++ src/components/molecules/ProfileMenu/index.tsx
@@ -1,16 +1,24 @@
+import { FiX } from 'react-icons/fi';
 import Header from '../../atoms/Header';
 import Styled from './styles';
 import MainTitle from '../../atoms/MainTitle';
 import ModalTemplate from '../../templates/Modal';
 import ProfileNavigation from '../ProfileNavigation';
+import IconButton from '../../atoms/IconButton';
+import theme from '../../../theme/theme';
 
 const ProfileMenu = ({ modalToggle }: { modalToggle: (val: boolean) => void }) => (
   <ModalTemplate isAdjustedToParent modalToggle={modalToggle}>
     <Header>
       <MainTitle>Voirsy</MainTitle>
-      <button type="button" onClick={() => modalToggle(false)}>
-        Close
-      </button>
+      <IconButton
+        onClick={() => modalToggle(false)}
+        aria-label="Close menu"
+        color="black"
+        size={theme.sizes.header.mobileHeight - 0.5}
+      >
+        <FiX />
+      </IconButton>
     </Header>
     <Styled.ModalContent>
       <ProfileNavigation />
--- src/components/templates/Header/index.tsx
+++ src/components/templates/Header/index.tsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from 'react';
-import { useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import { FiAlignJustify } from 'react-icons/fi';
 import { useAuthContextState } from '../../../context/authContext';
 import useModal from '../../../hooks/useModal';
 import Header from '../../atoms/Header';
+import IconButton from '../../atoms/IconButton';
 import MainTitle from '../../atoms/MainTitle';
 import ProfileMenu from '../../molecules/ProfileMenu';
 import Styled from './styles';
 import { HeaderTemplateTypes } from './types';
+import Button from '../../atoms/Button';
 
 const menuPaths = ['/profile', '/saloons'];
 
@@ -32,16 +35,39 @@ const HeaderTemplate = ({ children }: HeaderTemplateTypes) => {
         <Styled.ContentWrapper>
           {menuOptions.hasMenu ? (
             <Styled.MenuIconWrapper>
-              <button type="button" onClick={() => modalToggle(true)}>
-                Menu
-              </button>
+              <IconButton onClick={() => modalToggle(true)} aria-label="Open menu" color="black">
+                <FiAlignJustify size="70%" />
+              </IconButton>
             </Styled.MenuIconWrapper>
           ) : null}
           <MainTitle>Voirsy</MainTitle>
         </Styled.ContentWrapper>
-        <Styled.ContentWrapper>
-          {isAuthenticated() ? <p>{userInfo.name}</p> : <p>D U P A</p>}
-        </Styled.ContentWrapper>
+        <Styled.UserInfoContentWrapper>
+          {isAuthenticated() ? (
+            <>
+              <Styled.UserInfo>{userInfo.name}</Styled.UserInfo>
+              <Styled.AvatarWrapper>
+                <IconButton
+                  isAvatar
+                  aria-label="Open menu"
+                  avatarUrl={userInfo.avatarUrl ?? undefined}
+                />
+              </Styled.AvatarWrapper>
+            </>
+          ) : (
+            <Styled.ButtonWrapper>
+              <Button
+                variant="contained"
+                borderRadius={20}
+                as={Link}
+                to="/signin"
+                className="custom"
+              >
+                Sign in
+              </Button>
+            </Styled.ButtonWrapper>
+          )}
+        </Styled.UserInfoContentWrapper>
       </Header>
       {menuOptions.hasMenu && isModalOpen ? <ProfileMenu modalToggle={modalToggle} /> : null}
       {children}
