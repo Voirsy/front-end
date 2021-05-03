9019bc702667f75077947932235f10bbb75c6bbd
Sebastian <sebastian.zawolanski@gmail.com>
2021-05-03 20:52:54 +0200

    feat: set borderRadius prop to Button in DeleteAccount view

--- src/views/DeleteAccount/index.tsx
+++ src/views/DeleteAccount/index.tsx
@@ -6,7 +6,7 @@ const DeleteAccount = () => (
     <Styled.Notification>
       Are you sure to <strong>delete</strong> account? This process is irreversible.
     </Styled.Notification>
-    <Button color="warning" variant="contained" width={17} height={3.5}>
+    <Button color="warning" variant="contained" width={17} height={3.5} borderRadius={0.5}>
       Delete
     </Button>
   </>
